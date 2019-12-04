f86039d3a9b260865eca57e22521133f9e6c9d0b
Vicky-Raj <varthanv26018@gmail.com>
2019-12-04 20:40:30 +0530

    Changed URL Patterns

--- neworder/urls.py
+++ neworder/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('items/', views.ItemView.as_view()),
-    path('order/', views.OrderView.as_view())
+    path('order/', views.OrderView.as_view()),
+    path('edit/<int:pk>/',views.ItemEditView.as_view()),
+    path('delete/<int:pk>/',views.ItemDeleteView.as_view()),
+    path('customers/',views.CustomerSearchView.as_view())
 ]
