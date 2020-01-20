17b7fd98e80e0068fd6a4f9df250be5308515a34
Murphy <varthanv26018@gmail.com>
2020-01-20 14:26:01 +0530

    Latest Changes

---
+++ neworder/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .import views
+urlpatterns = [
+    path('items/', views.ItemView.as_view()),
+    path('order/', views.OrderView.as_view()),
+    path('customers/',views.CustomerSearchView.as_view()),
+    path('daily/',views.DailyItemView.as_view()),
+    path('subitems/',views.SubItemsView.as_view()),
+    path('orderid/',views.OrderByIdView.as_view())
+]
